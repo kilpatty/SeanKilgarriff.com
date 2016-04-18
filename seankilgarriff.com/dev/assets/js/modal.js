4a3b80c3bd89f0c0adbbd004a341856806807804
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-04-18 19:42:39 -0400

    added google analytics tracking to modal actions

--- seankilgarriff.com/dev/assets/js/modal.js
+++ seankilgarriff.com/dev/assets/js/modal.js
@@ -1,12 +1,33 @@
 $(function() {
+
 	$('#hair').on('click',function(){
 		$("#hair-modal-fade").addClass("active");
+			ga('send', {
+  				hitType: 'event',
+  				eventCategory: 'Modal',
+  				eventAction: 'viewed',
+  				eventLabel: 'Hair'
+			});
 	});
+
 	$('#glasses').on('click',function(){
 		$("#glasses-modal-fade").addClass("active");
+			ga('send', {
+  				hitType: 'event',
+  				eventCategory: 'Modal',
+  				eventAction: 'viewed',
+  				eventLabel: 'Glasses'
+			});
 	});
+
 	$('#beard').on('click',function(){
 		$("#beard-modal-fade").addClass("active");
+			ga('send', {
+  				hitType: 'event',
+  				eventCategory: 'Modal',
+  				eventAction: 'viewed',
+  				eventLabel: 'Beard'
+			});
 	});
 
 	// Remove the modal from any screen
