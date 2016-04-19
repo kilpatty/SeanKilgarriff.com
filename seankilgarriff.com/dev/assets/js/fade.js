982579a3240e0660fe119c1e544f707c4c423975
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-04-19 01:26:42 -0400

    renamed logoRow in fade in js

--- seankilgarriff.com/dev/assets/js/fade.js
+++ seankilgarriff.com/dev/assets/js/fade.js
@@ -1,5 +1,5 @@
 $(function() {
-	$(".logoRow").delay(7000).fadeIn(3000);
+	$(".logo-row").delay(7000).fadeIn(3000);
 	$(".footer").delay(7000).fadeIn(3000);
 	$(".welcome").delay(6000).fadeOut(1500);
 });
