f8c75430d9df0e1cb1b97f23a57d70b191b5f139
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-04-21 03:35:28 -0400

    cleaned up responsiveness for footer and changed fade

--- seankilgarriff.com/dev/assets/js/fade.js
+++ seankilgarriff.com/dev/assets/js/fade.js
@@ -1,5 +1,5 @@
 $(function() {
 	$(".logo-row").delay(7000).fadeIn(3000);
-	$(".footer").delay(7000).fadeIn(3000);
+	$(".fade").delay(7000).fadeIn(3000);
 	$(".welcome").delay(6000).fadeOut(1500);
 });
