6e381323893bdada20204242963cf0c8848614df
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-10 13:23:01 -0600

    move console messages into seperate file for now

--- src/index.js
+++ src/index.js
@@ -29,24 +29,6 @@ function logPageView() {
   ReactGA.pageview(window.location.pathname);
 }
 
-console.log(`%c
- __      __        __
-/  \    /  \ ____ |  |   ____  ____   _____   ____
-\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \
- \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/
-  \__/\  /  \___  >____/\___  >____/|__|_|  /\___  >
-       \/       \/          \/            \/     \/
-`);
-
-console.log(`%c ________________________________________
-< mooooooooooooooooooooooooooooooooooooo >
- ----------------------------------------
-        \\   ^__^
-         \\  (oo)\\_______
-            (__)\\       )\\/\\
-                ||----w |
-                ||     ||`, 'font-family:monospace');
-
 // Main Render Statement. <IndexRoute component={CheckPhone} />
 // <Route path="*" component={NotFound} />
 render(
