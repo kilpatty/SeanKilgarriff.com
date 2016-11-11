990538451a51cffeacb6acaf8150eccb8dc20833
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-11 12:11:21 -0600

    remove unneccesary components, and packages

--- src/assets/js/fade.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-	$(".logo-row").delay(7000).fadeIn(3000);
-	$(".fade").delay(7000).fadeIn(3000);
-	$(".welcome").delay(6000).fadeOut(1500);
-});
--- src/assets/js/home.js
+++
@@ -1,8 +0,0 @@
-!function (S, e, a, n, K) {S.GoogleAnalyticsObject = a; S[a] || (S[a]=function(){
-    (S[a].q=S[a].q||[]).push(arguments)});S[a].l=+new Date;n=e.createElement('script');
-    K=e.scripts[0];n.src='//www.google-analytics.com/analytics.js';
-    K.parentNode.insertBefore(n,K)}(window,document,'ga');
-
-    ga('create', 'UA-46649481-3', 'auto');
-	ga('require', 'outboundLinkTracker');
-    ga('send', 'pageview');
