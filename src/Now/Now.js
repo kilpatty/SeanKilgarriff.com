9cbbc960517019bc931f291c24913afa72be55a1
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-14 14:51:58 -0600

    format my now page a little bit better

--- src/Now/Now.js
+++ src/Now/Now.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 
 
 import styles from './Now.css';
@@ -13,11 +14,32 @@ const Now = () =>
        <main className={styles.container}>
          <div className={styles.content}>
            <p>
-          Hey everyone! Welcome to my now page! This is a pretty cool concept created by Derek
-          Sivers over at nownownow.com. It lets anyone who is visiting my website know what
-          I&#39;m currently up to. If you want to know more about the now page head on over to
-          Nownownow.com/about to learn more.
-          What I&#39;m currently working on:
+             Hey everyone! Welcome to my now page! This is a pretty cool concept created by Derek
+             Sivers over at <strong><ReactGA.OutboundLink
+               eventLabel="NowNowNow"
+               to="nownownow.com"
+             >nownownow.com</ReactGA.OutboundLink></strong>.
+             It lets anyone who is visiting my website know what
+             I&#39;m currently up to.
+          </p>
+           <p>
+            What I&#39;m currently working on:
+          </p>
+           <p>
+            Freelancing
+          </p>
+           <p>
+            Blogging @ blog.seankilgarriff.com
+          </p>
+           <p>
+            Blogging at Casual Mastery
+          </p>
+           <p>
+            If you want to know more about the now page head on over to
+             <strong><ReactGA.OutboundLink
+               eventLabel="NowNowNow"
+               to="nownownow.com/about"
+             > nownownow.com/about</ReactGA.OutboundLink></strong> to learn more.
          </p>
          </div>
        </main>
