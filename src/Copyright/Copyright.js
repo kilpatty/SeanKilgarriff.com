cb54198a578401dd029604963ce0cbc67def0124
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-14 14:10:39 -0600

    finish the copy for the copyright page

--- src/Copyright/Copyright.js
+++ src/Copyright/Copyright.js
@@ -20,31 +20,28 @@ const Copyright = () =>
        <main className={styles.container}>
          <div className={styles.content}>
            <p>
-           Everything on this website is handman by Sean Kilgarriff, and
+           This website is handmade by Sean Kilgarriff, and
            I would really appreciate you not stealing it!
           </p>
            <p>
-            Everything on this website is located on <ReactGA.OutboundLink
-              eventLabel="Github from Copyright"
-              to="https://github.com/Skilgarriff/SeanKilgarriff.com/"
-            >Github</ReactGA.OutboundLink>. Feel free to check it out and let me
-            know about any errors or typos!
-          </p>
-           <p>
-            This entire website is licensed under the <ReactGA.OutboundLink
+            This entire website is licensed under a <strong><ReactGA.OutboundLink
               eventLabel="MIT License"
               to="https://github.com/Skilgarriff/SeanKilgarriff.com/blob/master/LICENSE.md"
-            >MIT License</ReactGA.OutboundLink>.
-            All blog content and  videos are covered by licensed under a <ReactGA.OutboundLink
-              eventLabel="CC License"
-              to="https://creativecommons.org/licenses/by/4.0/"
-            >Creative Commons
-            Attribution 4.0 International License</ReactGA.OutboundLink>.
+            >MIT License</ReactGA.OutboundLink></strong>.
+            All blog content and  videos are covered by licensed under a <strong>
+              <ReactGA.OutboundLink
+                eventLabel="CC License"
+                to="https://creativecommons.org/licenses/by/4.0/"
+              >Creative Commons
+            Attribution 4.0 International License</ReactGA.OutboundLink></strong>.
           </p>
            <p>
-            You can read the license on Github, along with all of the other files
-            belonging to this website.
-           </p>
+           Everything on this website is located on <strong><ReactGA.OutboundLink
+             eventLabel="Github from Copyright"
+             to="https://github.com/Skilgarriff/SeanKilgarriff.com/"
+           >Github</ReactGA.OutboundLink></strong>. Feel free to check it out and let me
+           know about any errors or typos!
+         </p>
          </div>
        </main>
      </div>
