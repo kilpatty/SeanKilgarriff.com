1e182635b7daaf3644f38df8b580bc828031cee0
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-14 12:58:09 -0600

    clean up footer css and add it back into index

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,18 +1,15 @@
 // @flow
 
 import React from 'react';
-import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router';
-import ReactGA from 'react-ga';
 import styles from './Footer.css';
+
+// new Date().getFullYear() function for automatically generating the year for the copyright.
 
 const Footer = () =>
   (
     <footer className={styles.footer}>
-
-      <div className={styles.copyright}>
-        <Link to="/copyright">&copy; Sean Kilgarriff 2016</Link>
-      </div>
+      <Link to="/copyright">&copy; Sean Kilgarriff 2016</Link>
     </footer>
   );
 
--- src/index.js
+++ src/index.js
@@ -36,10 +36,14 @@ function logPageView() {
 render(
   <BrowserRouter onUpdate={logPageView}>
     <div className={styles.container}>
-      <Match exactly pattern="/" component={Home} />
-      <Match pattern="/copyright" component={Copyright} />
-      <Match pattern="/now" component={Now} />
-      <Footer />
+      <div className={styles.content}>
+        <Match exactly pattern="/" component={Home} />
+        <Match pattern="/copyright" component={Copyright} />
+        <Match pattern="/now" component={Now} />
+      </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
     </div>
   </BrowserRouter>,
 document.getElementById('app')
