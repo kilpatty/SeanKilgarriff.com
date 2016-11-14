8aecb11c2a290372136d85c96ddad2447bb791d4
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-14 14:05:05 -0600

    add licenses to copyright on my site fixes #44

--- src/Copyright/Copyright.js
+++ src/Copyright/Copyright.js
@@ -17,19 +17,33 @@ const Copyright = () =>
          </div>
        </nav>
 
-       <main>
+       <main className={styles.container}>
          <div className={styles.content}>
            <p>
            Everything on this website is handman by Sean Kilgarriff, and
-           he would really appreciate you not stealing it!
+           I would really appreciate you not stealing it!
           </p>
            <p>
-            This entire website is covered by the <ReactGA.OutboundLink
-              eventLabel="Medium"
-              to="https://medium.com/@SKilgarriff"
-              className={styles.socialLink}
-            >... ... License</ReactGA.OutboundLink>.
-            You can read the license on Github, along with all of the other files belonging to this website.
+            Everything on this website is located on <ReactGA.OutboundLink
+              eventLabel="Github from Copyright"
+              to="https://github.com/Skilgarriff/SeanKilgarriff.com/"
+            >Github</ReactGA.OutboundLink>. Feel free to check it out and let me
+            know about any errors or typos!
+          </p>
+           <p>
+            This entire website is licensed under the <ReactGA.OutboundLink
+              eventLabel="MIT License"
+              to="https://github.com/Skilgarriff/SeanKilgarriff.com/blob/master/LICENSE.md"
+            >MIT License</ReactGA.OutboundLink>.
+            All blog content and  videos are covered by licensed under a <ReactGA.OutboundLink
+              eventLabel="CC License"
+              to="https://creativecommons.org/licenses/by/4.0/"
+            >Creative Commons
+            Attribution 4.0 International License</ReactGA.OutboundLink>.
+          </p>
+           <p>
+            You can read the license on Github, along with all of the other files
+            belonging to this website.
            </p>
          </div>
        </main>
