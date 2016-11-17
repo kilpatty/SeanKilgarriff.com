c26824887cec57accca63c6373c68a774ec1a6ec
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-17 16:02:54 -0600

    some base text for the about page

--- src/About/About.js
+++ src/About/About.js
@@ -15,7 +15,18 @@ const About = () =>
          <div className={styles.content}>
            <h1>Who I Am</h1>
            <p>
-             Hey there! Welcome to my website. Glad to have you!
+             Hey there! Welcome to my website. Glad to have you! My name is
+             Sean Kilgarriff. For work I primarily design and develop websites,
+            but I dabble in a lot of things. I am a coffee fanatic, and I am an
+            avid traveler.
+
+            This is my personal website. I did not design this like I would a typical website.
+            There are a lot of hidden gems, so look around and enjoy the adventure!
+
+            If you want to find out what I am currently up to, then check out my Now page.
+
+            Contact Me
+
            </p>
 
          </div>
