d849f26eb3fad77c3cbf6b51a08ca5a9ab13f24a
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-17 14:12:07 -0600

    boilerplate for the about page

--- src/About/About.js
+++ src/About/About.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
 
 import styles from './About.css';
 
@@ -10,8 +12,13 @@ const About = () =>
        <Navigation />
 
        <main className={styles.container}>
-         <div className={styles.content} />
-         hey
+         <div className={styles.content}>
+           <h1>Who I Am</h1>
+           <p>
+             Hey there! Welcome to my website. Glad to have you!
+           </p>
+
+         </div>
        </main>
      </div>
   )
