f247ef5d2e912d34fe1264f469a1c694acef507b
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-17 20:41:29 -0600

    add work in progress for portfolio page

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import styles from './Portfolio.css';
+
+import Navigation from '../Navigation';
+
 const Portfolio = () =>
    (
-     <div />
+     <div className={styles.wrapper}>
+       <Navigation />
+
+       <main className={styles.container}>
+         <div className={styles.content}>
+           Work in Progress
+         </div>
+       </main>
+     </div>
   )
 ;
 
--- src/index.js
+++ src/index.js
@@ -94,6 +94,45 @@ ifvisible.on('blur', () => {
   console.log('hidden');
 });
 
+console.log(`
+  MMMMMMMMMWNKOdl:;,''''''';:lx0NWMMMMMMMM
+  MMMMN0KXOdc;'...............',:oOXWMMMMM
+  MMWKl,;;'........................;oONMMM
+  MMXl...............................'cONM
+  MNk,.................................,dX
+  0l,..................................;xK
+  x:.......;ldxxdolc;,''...............lXM
+  Xd'...'ckXWMMMMMMWNKK0Oxol:,'.......,xWM
+  Wx,...oNMMMMMMMMMMMMMMMMMWNKOxl;....:0MM
+  WO;..'xWMMMMMMMMMMMMMMMMMMMMMMWXo'.'oNMM
+  MXc..'kMMMMMMMMMMMMMMMMMMMMMMMMMO,.,OWMM
+  MWx'.,kWNXXXXXXXXNMWWNXXXXXXXXNW0;.lXMMM
+  MMK:.;k0OKKKKKKKOk0Oxk0KKKKKK0OXO;,kWMMM
+  MMWx,:OO0MMMMMMMKkOkoOWMMMMMMN0KO;lXMMMM
+  MMMXllKK0KKKKKKKO0NX000KKKKKK00XOlOMMMMM
+  MMMM0ONMWNNNNNNNNWMMMWNNNNNNNNWMXKWMMMMM
+  MMMMKd0MMMMMMMMMMMMMMMMMMMMMMMMNxxNMMMMM
+  MMMMKcoNMMMMMMWWNNNNNNNWMMMMMMM0:oNMMMMM
+  MMMMKc;xNMMNOdlc::;:::clox0WMWXo'dWMMMMM
+  MMMM0;.'cxd:'.............,lxd:..lXMMMMM
+  MMMNo........;oddxOOkxddl,.......,kWMMMM
+  MMM0;........;ldxxxxxxxoc,........lXMMMM
+  MMM0;.............................lXMMMM
+  MMMNd'...........................,kWMMMM
+  MMMM0:...........................lXMMMMM
+  MMMMNo..........................,kWMMMMM
+  MMMMMKdc;'...................,;lkNMMMMMM
+  MMMMMWNNX0kdolc:;;;;;;;:cldxOKXNNWMMMMMM
+  MMMMWkccldk0XNNNNXXXXNNNWWX0kdlcckNMMMMM
+  MMMM0:.....';lxKWMMMMMWXkl;'.....;OMMMMM
+  MMMWx'.........,ldxxxdl;..........dWMMMM
+  MMMNd'............................lXMMMM
+  MMMNd'............................oNMMMM
+  MMMMk,........';okOOOkd:'........'xWMMMM
+  MMMMKc....',:oOXWMMMMMWNOdc,'....:0MMMMM
+  MMMMWk,,:lx0NWMMMMMMMMMMMWNKxl:,,dNMMMMM
+`);
+
 devtoolsWelcome();
 
 render(
