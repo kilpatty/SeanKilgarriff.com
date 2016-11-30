687aff14840a0a8ec945b70c0bc7f730863270bb
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-29 23:59:20 -0600

    add more properties to be passed down to components

--- src/GAMatch/GAMatch.js
+++ src/GAMatch/GAMatch.js
@@ -3,10 +3,10 @@ import { Match } from 'react-router';
 import ReactGA from 'react-ga';
 
 
-const GAMatch = ({ component: Comp, ...props }) => {
+const GAMatch = ({ component: Comp, passProps, ...props }) => {
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
-  return <Match {...props} render={(matchedProps) => <Comp {...matchedProps} />} />;
+  return <Match {...props} render={(matchedProps) => <Comp {...passProps} {...matchedProps} />} />;
 };
 
 export default GAMatch;
