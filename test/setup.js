53dd63a847382ad89dcfa19b37fba9e00c1266ec
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-12-13 04:14:58 -0600

    add packages for test coverage with instabul

--- test/setup.js
+++ test/setup.js
@@ -2,6 +2,21 @@
 
 const jsdom = require('jsdom').jsdom;
 
+process.env.NODE_ENV = 'test';
+
+function noop() {
+  return null;
+}
+
+require.extensions['.css'] = noop;
+require.extensions['.scss'] = noop;
+require.extensions['.md'] = noop;
+require.extensions['.png'] = noop;
+require.extensions['.svg'] = noop;
+require.extensions['.jpg'] = noop;
+require.extensions['.jpeg'] = noop;
+require.extensions['.gif'] = noop;
+
 global.document = jsdom('');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
