5d868d8422e885e0e171dc50de9cb61e6a3e50ab
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-12-13 15:27:50 -0600

    clean up footer test and change the naming convention slightly

--- src/Components/Footer/Footer.spec.js
+++ src/Components/Footer/Footer.spec.js
@@ -1,6 +1,3 @@
-/* eslint-env mocha */
-/* eslint-disable padded-blocks, no-unused-expressions */
-
 import React from 'react';
 import { Link } from 'react-router';
 import { expect } from 'chai';
@@ -8,27 +5,18 @@ import { shallow } from 'enzyme';
 import Footer from './Footer';
 
 describe('Footer', () => {
-
   it('Should have a link to Copyright', () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.contains(<Link to="/Copyright"> Sean Kilgarriff Original &copy; 2016</Link>)).to.equal(true);
-    expect(wrapper.find(Link)).to.have.length(1);
+    const footer = shallow(<Footer />);
+    expect(footer.find(Link)).to.have.length(1);
   });
 
-  it('Shoudl render a footer element', () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.find('footer')).to.have.length(1);
+  it('Should render a footer element', () => {
+    const footer = shallow(<Footer />);
+    expect(footer.find('footer')).to.have.length(1);
   });
 
-
-  // it('should have an image to display the gravatar', () => {
-  //   const wrapper = shallow(<Avatar />);
-  //   expect(wrapper.find('img')).to.have.length(1);
-  // });
-  //
-  // it('should have props for email and src', () => {
-  //   const wrapper = shallow(<Avatar />);
-  //   expect(wrapper.props().email).to.be.defined;
-  //   expect(wrapper.props().src).to.be.defined;
-  // });
+  it('Should render the proper copyright text', () => {
+    const footer = shallow(<Footer />);
+    expect(footer.contains(<Link to="/Copyright"> Sean Kilgarriff Original &copy; 2016</Link>)).to.equal(true);
+  });
 });
