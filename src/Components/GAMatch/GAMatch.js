8c68801afbdf40a4c5649a2430e3c8ffca22d50d
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-12-20 01:35:15 -0600

    wip testing framework

--- src/Components/GAMatch/GAMatch.js
+++ src/Components/GAMatch/GAMatch.js
@@ -4,7 +4,6 @@ import ReactGA from 'react-ga';
 
 
 const GAMatch = ({ component: Comp, passProps, ...props }) => {
-  console.log(typeof Comp)
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
   return <Match {...props} render={(matchedProps) => <Comp {...passProps} {...matchedProps} />} />;
