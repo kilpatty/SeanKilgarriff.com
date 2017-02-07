e1d2a1239ba841c17821f0a380896277672f1cbc
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2017-02-06 22:23:52 -0600

    update now page text

--- src/Screens/Now/Now.js
+++ src/Screens/Now/Now.js
@@ -27,41 +27,44 @@ const Now = () =>
            <h1>
             What I&#39;m currently working on:
            </h1>
-           <h2>Freelancing & Traveling</h2>
+           <h2>
+            Leadpages
+           </h2>
+           <p>
+            As of January, I am <strong><ReactGA.OutboundLink
+              eventLabel="Leadpages-Now"
+              to="https://leadpages.net"
+            >Leadpages&#39; </ReactGA.OutboundLink></strong> new Site Reliability Engineer.
+             I will maintaining and building their CI/CD pipeline as well as transitioning
+             services to Kubernetes. I am extremely excited to be working for a such a great
+             company with a great culture!
+            </p>
+           <h2>Blogging On This Site</h2>
+           <p>
+              Over the past few years, I have had a lot that I have wanted to write about,
+              but I could never find a medium that I was completely satisfied with.
+              Creating my own blog has given me a place to write about all things that I
+              am interested in whether it&#39;s politics, programming, or just general lifestyle.
+              If you want to see my latest posts, head over to my <strong><ReactGA.OutboundLink
+                eventLabel="Blog-Now"
+                to="https://blog.seankilgarriff.com"
+              > blog</ReactGA.OutboundLink></strong> and signup for my newsletter!
+            </p>
+           <h2>Traveling & Open Source</h2>
            <p>
               I am currently traveling to as many places as I can, while also
-              doing some freelancing projects on the side.
+              doing some open source projects on the side.
               If you want to follow me on my travels, checkout my
               <strong><ReactGA.OutboundLink
                 eventLabel="Instagram-Now"
                 to="https://www.instagram.com/skilgarriff/"
               > Instagram</ReactGA.OutboundLink></strong>, where I will regularly
-              post photos of where I am.
-           </p>
-           <h2>
-            Casual Mastery
-           </h2>
-           <p>
-            I&#39;ve always been interested in learning and mastering new things everyday.
-            I&#39;ll spend hours reading about Japanese Mud-Ball shaping one day
-            (<strong><ReactGA.OutboundLink
-              eventLabel="Dorodango-Now"
-              to="https://en.wikipedia.org/wiki/Dorodango"
-            >Dorodango</ReactGA.OutboundLink></strong> for those interested),
-            and then Coffee grown by Monks the next day.
-            Casual Mastery is a place where I will post about my adventures in gaining
-            a casual mastery over certain subjects.
-            </p>
-           <h2>Blogging On This Site</h2>
-           <p>
-             Over the past few years, I have had a lot that I have wanted to write about,
-             but I could never find a medium that I was completely satisfied with.
-             Creating my own blog has given me a place to write about all things that I
-             am interested in whether it&#39;s politics, programming, or just general lifestyle.
-             If you want to see my latest posts, head over to my <strong><ReactGA.OutboundLink
-               eventLabel="Blog-Now"
-               to="https://blog.seankilgarriff.com"
-             > blog</ReactGA.OutboundLink></strong> and signup for my newsletter!
+              post photos of where I am. If you want to see some of the projects I&#39;m
+              currently working on, head over to my <strong><ReactGA.OutboundLink
+                eventLabel="Github-Now"
+                to="https://github.com/Skilgarriff"
+              > Github</ReactGA.OutboundLink></strong>.
+
            </p>
            <p>
             If you want to know more about the now page head on over to
